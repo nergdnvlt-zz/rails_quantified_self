b1cd0c415d18ebcc9b37f676263af8d2d2e94cd0
nergdvnlt <tylerlundgren@yahoo.com>
2018-06-26 09:00:03 -0600

    Adding initial test with functionalit, going to build other test for multiple items and sad path.

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::FoodsController < ApplicationController
   def index
     render json: Food.all.as_json
   end
+
+  def show
+    render json: Food.find(params[:id]).as_json
+  end
 end
