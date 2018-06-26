eb820e842f617e8dcf561110fb4befd3f5b79512
nergdvnlt <tylerlundgren@yahoo.com>
2018-06-26 17:36:32 -0600

    Adding testing for delete sad path and delete.

--- spec/requests/api/v1/foods/delete_food_spec.rb
+++ spec/requests/api/v1/foods/delete_food_spec.rb
@@ -10,4 +10,14 @@ describe 'delete request for a food' do
       expect(response.status).to eq(204)
     end
   end
+
+  describe 'cant find the food to delete' do
+    it 'and gives a 404 response' do
+      pear = Food.create(name: 'pear', calories: 33)
+
+      delete "/api/v1/foods/88"
+
+      expect(response.status).to eq(404)
+    end
+  end
 end
