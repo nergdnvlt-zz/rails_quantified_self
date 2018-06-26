bf7f3bcaa9eb9d6f39e59803ed4c6f225c0de67c
nergdvnlt <tylerlundgren@yahoo.com>
2018-06-26 17:11:13 -0600

    Adding testing for patch sad path.

--- spec/requests/api/v1/foods/patch_request_to_foods_spec.rb
+++ spec/requests/api/v1/foods/patch_request_to_foods_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'a get request to foods#index' do
   describe 'when processed correctly' do
-    it 'posts a new food to JSON' do
+    it 'patches updated food as JSON' do
       food = create(:food)
       patch "/api/v1/foods/#{food.id}", params: { "food": { "name": "peach", "calories": "98"} }
 
@@ -13,6 +13,24 @@ describe 'a get request to foods#index' do
       expect(apricot_response).to be_a Hash
       expect(apricot_response["name"]).to eq("peach")
       expect(apricot_response["calories"]).to eq(98)
+    end
+  end
+
+  describe 'when not processed correctly' do
+    it 'gives 404 response code when name is missing' do
+      food = create(:food)
+      patch "/api/v1/foods/#{food.id}", params: { "food": { "calories": "98"} }
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+    end
+
+    it 'gives 404 response code when calories are missing' do
+      food = create(:food)
+      patch "/api/v1/foods/#{food.id}", params: { "food": { "name": "peach" } }
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
     end
   end
 end
