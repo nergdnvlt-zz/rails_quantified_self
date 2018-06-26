361fe466c09a4cfcf98b94680984d50deee66b69
nergdvnlt <tylerlundgren@yahoo.com>
2018-06-26 16:44:21 -0600

    Testing for sad path for foods posting endpoint.

--- spec/requests/api/v1/foods/post_request_to_foods_spec.rb
+++ spec/requests/api/v1/foods/post_request_to_foods_spec.rb
@@ -15,16 +15,21 @@ describe 'a get request to foods#index' do
     end
   end
 
-  # describe 'when not processed correctly' do
-  #   it 'gives a error response code' do
-  #     post "/api/v1/foods", params: { "food": { "name": "apricot", "calories": "88"} }
-  #
-  #     expect(response).to be_successful
-  #
-  #     apricot_response = JSON.parse(response.body)
-  #
-  #     expect(response).to_not be_successful
-  #     expect(response.status).to eq(404)
-  #   end
-  # end
+  describe 'when not processed correctly' do
+    it 'gives 404 response code when name is missing' do
+      post "/api/v1/foods", params: { "food": { "calories": "88" } }
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+    end
+  end
+
+  describe 'when not processed correctly' do
+    it 'gives 404 response code when calories are missing' do
+      post "/api/v1/foods", params: { "food": { "name": "jerky" } }
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+    end
+  end
 end
