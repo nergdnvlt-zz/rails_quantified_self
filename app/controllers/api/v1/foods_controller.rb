089a4985f80f3a41356be1d23c6cd9a1301a57c1
nergdvnlt <tylerlundgren@yahoo.com>
2018-06-26 13:11:46 -0600

    Adding post method for foods.

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -6,4 +6,14 @@ class Api::V1::FoodsController < ApplicationController
   def show
     render json: Food.find(params[:id]).as_json
   end
+
+  def create
+    render json: Food.create(food_params).as_json
+  end
+
+  private
+  
+  def food_params
+    params.require(:food).permit(:name, :calories)
+  end
 end
